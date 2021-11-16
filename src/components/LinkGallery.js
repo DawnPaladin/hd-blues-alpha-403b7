1a00dcc336b1613b06e57a435deb64657aeb9d12
Stackbit Code Editor <projects@stackbit.com>
2021-11-16 22:20:47 +0000

    LinkGallery.js: changed

--- src/components/LinkGallery.js
+++ src/components/LinkGallery.js
@@ -2,7 +2,7 @@ import { getComponent } from '../../components-registry';
 
 function ImageLink(model) {
     const Component = getComponent(model.type);
-    return <Component {...feature}
+    return <Component {...feature} />;
 }
 
 export function LinkGallery(props) {
