5bd32e268a639fc521f3d9526bee4ca757621663
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 23:14:05 +0000

    ImageLink.js: changed

--- src/components/ImageLink.js
+++ src/components/ImageLink.js
@@ -1,6 +1,7 @@
 const ImageLink = ({ imgUrl, link, altText }) => {
     return (
         <a href={link}>
+            I'm an image link
             <img src={imgUrl} alt={altText} />
         </a>
     )
