db7b83395969d22421a775134cd7af4b2434e619
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 04:31:25 +0000

    LinkGallery.js: changed

--- src/components/LinkGallery.js
+++ src/components/LinkGallery.js
@@ -8,7 +8,7 @@ function LinkGallery(props) {
         <div class="row">
             <h2>{props.title}</h2>
             <p>{proptypes}</p>
-            <ImageLink link={props.ImageLink}/>
+            <ImageLink link="link"/>
         </div>
     );
 }
