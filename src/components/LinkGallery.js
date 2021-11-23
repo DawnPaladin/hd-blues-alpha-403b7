6e6e29727aaa554827afc710cdd6501cffa4937b
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 04:59:06 +0000

    LinkGallery.js: changed

--- src/components/LinkGallery.js
+++ src/components/LinkGallery.js
@@ -6,7 +6,7 @@ function LinkGallery(props) {
     const proptypes = Object.keys(props).join(",");
     const imageLinkData = props.imageLinks;
     const imageLinks = imageLinkData.map(imageLink => (
-        
+        <ImageLink link={ImageLink}.
     ))
     return (
         <div class="row">
