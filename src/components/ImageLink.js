43c6d4a3d2dd29ede38a06b67c83358d24fa7fec
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 05:25:56 +0000

    ImageLink.js: changed

--- src/components/ImageLink.js
+++ src/components/ImageLink.js
@@ -2,7 +2,6 @@ const ImageLink = props => {
     const link = props.link || "";
     const imgUrl = props.imgUrl || "";
     const altText = props.altText || "";
-    const proptypes = Object.keys(props).join(",");
     return (
         <a href={link}>
             <img src={imgUrl} alt={altText} />
