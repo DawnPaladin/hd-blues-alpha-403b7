482350c1bd1478a932ef2f4f9078609422c63290
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 03:27:09 +0000

    LinkGallery.js: changed

--- src/components/LinkGallery.js
+++ src/components/LinkGallery.js
@@ -3,6 +3,7 @@ import { getComponent } from '@stackbit/components/dist/components-registry';
 function LinkGallery(props) {
     const imageLinks = props.imageLinks || [];
     const ImageLink = getComponent("ImageLink");
+    
     return (
         <div class="row">
             <h2>{props.title}</h2>
