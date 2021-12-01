71db5a9b39afc33a4166e8ac5323e27e7ab33a0b
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 23:19:04 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -10,7 +10,7 @@ registerComponents({
     // or register your own dynamic component.
     ImageLink: dynamic(() => import('./ImageLink')),
     LinkGallery: dynamic(() => import('./LinkGallery')),
-    Flexbox
+    Flexbox: dynamic(() => import('./Flexbox')
 });
 
 /**
