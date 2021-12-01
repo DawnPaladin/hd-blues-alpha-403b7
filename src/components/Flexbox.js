b79a7305d609cd71407d55e5450d1736b82c9dbc
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 23:17:11 +0000

    Flexbox.js: changed

---
+++ src/components/Flexbox.js
@@ -0,0 +1,19 @@
+import { getComponent } from '@stackbit/components/dist/components-registry';
+
+function LinkGallery(props) {
+    const ImageLink = getComponent("ImageLink");
+    const proptypes = Object.keys(props).join(",");
+    const imageLinkData = props.imageLinks || [];
+    const imageLinks = imageLinkData.map(imageLink => (
+        <ImageLink link={imageLink.link} image={imageLink.image} altText={imageLink.altText} />
+    ))
+    return (
+        <div class="row">
+            <h2>{props.title}</h2>
+            <p>{proptypes}</p>
+            {imageLinks}
+        </div>
+    );
+}
+
+export default LinkGallery
