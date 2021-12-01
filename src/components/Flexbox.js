760a9f5c508df88820048d250cc56297f82b44c6
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 23:22:08 +0000

    Flexbox.js: changed

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -1,7 +1,7 @@
-const Flexbox = ({ body }) => {
+const Flexbox = ({ text }) => {
     return (
         <div className="flexbox">
-            { body }
+            { text }
         </div>
     )
 }
