4236f974f0ad96276dde29179f0c16c8de8459c2
Stackbit Code Editor <projects@stackbit.com>
2021-12-07 20:45:24 +0000

    Flexbox.js: changed

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -4,7 +4,7 @@ import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/ut
 
 const Flexbox = props => {
     const styles = props.styles || {}
-    const className = classNames("flex", mapMaxWidthStyles(styles?.self?.width), )
+    const className = classNames("flex", mapMaxWidthStyles(styles?.self?.width), mapStyles())
     console.warn(styles)
     return (
         <div className={className} data-sb-field-path={props.annotationPrefix}>
