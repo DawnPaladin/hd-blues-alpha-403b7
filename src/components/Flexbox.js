bdfddadddeea805c05781d250ff728844ef9caa2
Stackbit Code Editor <projects@stackbit.com>
2021-12-07 20:42:20 +0000

    Flexbox.js: changed

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -1,6 +1,6 @@
 import { getComponent } from '@stackbit/components/dist/components-registry';
 import classNames from 'classnames'
-// import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names'
+import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names'
 
 const Flexbox = props => {
     const styles = props.styles || {}
